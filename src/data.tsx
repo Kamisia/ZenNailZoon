83cc1dc99d3eaabdc95cb4283ac474810b60b06d
Kamisia <kamila.samczuk@gmail.com>
2024-06-03 10:19:38 +0200

    Added data.tsx and images

---
+++ src/data.tsx
@@ -0,0 +1,71 @@
+import employ1 from "./assets/employer1.jpg";
+import employ2 from "./assets/employer2.jpg";
+
+export const portfolioImg = [
+  {
+    id: 1,
+
+    imageUrl: `${employ1}`,
+  },
+  {
+    id: 2,
+
+    imageUrl: `${employ1}`,
+  },
+  {
+    id: 3,
+
+    imageUrl: `${employ1}`,
+  },
+  {
+    id: 4,
+
+    imageUrl: `${employ1}`,
+  },
+  {
+    id: 5,
+
+    imageUrl: `${employ1}`,
+  },
+  {
+    id: 6,
+
+    imageUrl: `${employ1}`,
+  },
+  {
+    id: 7,
+
+    imageUrl: `${employ1}`,
+  },
+  {
+    id: 8,
+
+    imageUrl: `${employ1}`,
+  },
+];
+export const links = [
+  { id: 1, url: "#home", text: "Home" },
+  { id: 2, url: "#ourservice", text: "Our Service" },
+  { id: 3, url: "#about", text: "About" },
+  { id: 4, url: "#contact", text: "Contact" },
+];
+
+export const employer = [
+  {
+    id: 1,
+    imageUrl: `${employ1}`,
+    title: "Elena Nice",
+    text: " I'm Elena Nica, dedicated to creating nail art that speaks volumes about your personality and style. With a keen eye for detail and a passion for creativity, I strive to provide my clients with exceptional nail care experiences tailored to their preferences. From intricate designs to timeless classics, I'm committed to delivering flawless nails that leave a lasting impression.",
+  },
+  {
+    id: 2,
+    imageUrl: `${employ2}`,
+    title: "Amanda John",
+    text: " I'm Amanda John, dedicated to creating nail art that speaks volumes about your personality and style. With a keen eye for detail and a passion for creativity, I strive to provide my clients with exceptional nail care experiences tailored to their preferences. From intricate designs to timeless classics, I'm committed to delivering flawless nails that leave a lasting impression.",
+  },
+];
+export const service = [
+  { id: 1, title: "", text: "", imageUrl: "" },
+  { id: 2, title: "", text: "", imageUrl: "" },
+  { id: 3, title: "", text: "", imageUrl: "" },
+];
