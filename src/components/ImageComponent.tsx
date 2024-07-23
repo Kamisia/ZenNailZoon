af8b2b3043a0730b7e21647db52f91102dbc812e
Kamisia <kamila.samczuk@gmail.com>
2024-07-23 12:34:11 +0200

    Changing the display in ImageComponent.tsx

--- src/components/ImageComponent.tsx
+++ src/components/ImageComponent.tsx
@@ -5,13 +5,24 @@ interface ImageComponentProps {
 }
 const ImageComponent: React.FC<ImageComponentProps> = ({ url, author }) => {
   return (
-    <div className="flex justify-center ">
-      <img className="rounded-lg w-80 max-w-fit max-h-fit " src={url} alt="" />
+    <div
+      className="flex justify-center bg-container bg-no-repeat bg-center w-full h-96"
+      style={{ backgroundImage: `url(${url})` }}
+    >
       {author && (
-        <div className="bg-rose-700 bg-opacity-5 w-full absolute ">
+        <div className="bg-rose-800 bg-opacity-60 w-full absolute ">
           <p className="text-amber-50">By {author}</p>
         </div>
       )}
+
+      {/*<div className="flex justify-center ">
+  <img className="rounded-lg w-80 " src={url} alt="" />
+  {author && (
+    <div className="bg-rose-700 bg-opacity-5 w-full absolute ">
+      <p className="text-amber-50">By {author}</p>
+    </div>
+  )}
+</div>*/}
     </div>
   );
 };
