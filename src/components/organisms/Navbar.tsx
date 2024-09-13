e1dfb7a34f52a717aa0925bddd946afc1d74c576
Kamila Samczuk <kamila.samczuk@gmail.com>
2024-09-13 12:13:24 +0200

    Update Navbar.tsx

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar: React.FC = () => {
   return (
     <div className="fixed z-20 top-0 content-center  h-12 rounded-b-lg  flex-row flex justify-between mr-auto ml-auto w-full">
       <div className="fixed z-100 top-0 content-center text-amber-50 text-sm bg-gradient-to-r from-rose-500 to-rose-400  h-12 p-3 flex-row flex justify-between mr-auto ml-auto w-screen">
-        <LogoComponent text={"ZenNailZoon"} />
+        <LogoComponent text={"ZenNailZone"} />
 
         <nav className=" hidden flex-row gap-4 md:flex ml-2">
           {links.map((link) => {
